e3c436c8c9601b0ab239e43cf2de0a4c1997c338
Saymon85 <saymon_bg@hotmail.com>
2019-02-18 00:01:32 +0100

    Adding logic for validation and event listeners

--- validation.js
+++ validation.js
@@ -1,1 +1,19 @@
-// validation script here
+const inputs = document.querySelectorAll('input');
+const patterns = {
+    telephone: /^\d{11}$/,
+}
+
+function testRegExp(testField, regex){
+    if(regex.test(testField.value)){
+        testField.className = "valid";
+    }
+    else{
+        testField.className = "invalid";
+    }
+}
+
+inputs.forEach((el) => {
+    el.addEventListener('blur', (e) => {
+        testRegExp(e.target, patterns[e.target.attributes.name.value]);
+    })
+})
